918f459c34098e73b1c76b702c8875be0751fe53
KhaledAbdellaty <kabdellaty@gmail.com>
2024-07-31 02:29:42 +0300

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale():
 def get_index() -> str:
     """The home/index page.
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
